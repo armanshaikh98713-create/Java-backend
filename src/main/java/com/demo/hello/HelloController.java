bfd51ac4c7e9b7f8313dfb797ae7215d6c5c64c1
shkarman12 <shaikharman79000@gmail.com>
2026-03-31 22:30:51 +0530

    Hello Armaan!

--- src/main/java/com/demo/hello/HelloController.java
+++ src/main/java/com/demo/hello/HelloController.java
@@ -22,7 +22,7 @@ public class HelloController {
     @GetMapping("/hello")
     public Map<String, String> hello() {
         return Map.of(
-            "message", "Hello, World!",
+            "message", "Hello, Armaan!!",
             "status",  "success"
         );
     }
