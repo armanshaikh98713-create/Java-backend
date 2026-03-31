61d134f6bd0b8a4a474d48cd91e4b5410d8dce6b
shkarman12 <shaikharman79000@gmail.com>
2026-03-31 22:36:02 +0530

    Hello Armaan!

--- src/test/java/com/demo/hello/HelloControllerTest.java
+++ src/test/java/com/demo/hello/HelloControllerTest.java
@@ -20,7 +20,7 @@ class HelloControllerTest {
     void helloEndpointReturnsSuccess() throws Exception {
         mockMvc.perform(get("/api/hello"))
                .andExpect(status().isOk())
-               .andExpect(jsonPath("$.message").value("Hello, World!"))
+               .andExpect(jsonPath("$.message").value("Hello, Armaan!!"))
                .andExpect(jsonPath("$.status").value("success"));
     }
 
